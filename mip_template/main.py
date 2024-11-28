9d7be7cc4678d2268698379d4d6bcf4acddd0d75
Eder PInheiro <eder.pinheiro@mipwise.com>
2024-11-27 22:33:46 -0300

    feature: use parameter error from mip-utils

--- mip_template/main.py
+++ mip_template/main.py
@@ -1,3 +1,5 @@
+from mip_utils.exceptions import ParameterError
+
 from mip_template.schemas import input_schema, output_schema
 from mip_utils.ticdat_utils import set_data_types, set_parameters_datatypes
 
@@ -13,7 +15,7 @@ def solve(dat):
     elif params['Sample Two Values Parameter'] == 'Value 2':
         sample_output_table_df = sample_input_table_df[['Primary Key Two', 'Data Field Two']]
     else:
-        raise ValueError(f"bad value for 'Sample Two Values Parameter': {params['Sample Two Values Parameter']}")
+        raise ParameterError(f"bad value for 'Sample Two Values Parameter': {params['Sample Two Values Parameter']}", ValueError)
     sample_output_table_df.rename(
         columns={'Primary Key One': 'Primary Key', 'Data Field One': 'Data Field'}, inplace=True)
     sln = output_schema.PanDat()
