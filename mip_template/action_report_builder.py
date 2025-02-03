1ecba5c49b181a2fb18d3bf082538cf73d701127
Eder Pinheiro <eder.pinheiro@mipwise.com>
2025-02-03 18:00:16 -0300

    feature: add dev dependencies jupyterlab and ruff.

--- mip_template/action_report_builder.py
+++ mip_template/action_report_builder.py
@@ -7,7 +7,7 @@ from plotly.graph_objs import Figure
 from mip_template.constants import APP_OUTPUT_DIR
 
 
-def _save_plot(fig: Figure, plot_name: str, path: str=APP_OUTPUT_DIR):
+def _save_plot(fig: Figure, plot_name: str, path: str = APP_OUTPUT_DIR):
     """Save plots, as HTML, to the default directory of Mip Hub.
 
     When executed locally, saves the HTML file to app/output/ (default directory of Mip Hub), or to the specified path.
@@ -16,7 +16,7 @@ def _save_plot(fig: Figure, plot_name: str, path: str=APP_OUTPUT_DIR):
     :param plot_name: Name of the plot to be saved as an HTML file and to be displayed on Mip Hub.
     :param path: Path to the output
     """
-    file_path = os.path.join(f'{path}/{plot_name}.html')
+    file_path = os.path.join(f"{path}/{plot_name}.html")
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     fig.write_html(file_path)
 
@@ -25,18 +25,23 @@ def report_builder_solve(dat, sln, path=APP_OUTPUT_DIR):
     """Sample output action."""
     sample_input_table_df = dat.sample_input_table.copy()
     sample_output_table_df = sln.sample_output_table.copy()
-    sample_output_table_df['Data Field'] = sample_input_table_df['Data Field One'] + '.0'
+    sample_output_table_df["Data Field"] = (
+        sample_input_table_df["Data Field One"] + ".0"
+    )
     # region build plots
     kpis_df = sample_input_table_df.copy()
-    kpis_values = list(zip(kpis_df['Primary Key One'], kpis_df['Data Field Two']))
-    fig = go.Figure(go.Bar(
-        x=[value for kpi, value in kpis_values],
-        y=[kpi for kpi, value in kpis_values],
-        orientation='h'))
-    fig.update_layout(title='Costs Breakdown')
-    _save_plot(fig, 'KPISummary', path)
+    kpis_values = list(zip(kpis_df["Primary Key One"], kpis_df["Data Field Two"]))
+    fig = go.Figure(
+        go.Bar(
+            x=[value for kpi, value in kpis_values],
+            y=[kpi for kpi, value in kpis_values],
+            orientation="h",
+        )
+    )
+    fig.update_layout(title="Costs Breakdown")
+    _save_plot(fig, "KPISummary", path)
     fig = ff.create_table(sample_output_table_df)
-    _save_plot(fig, 'TablePlot', path)
+    _save_plot(fig, "TablePlot", path)
     # endregion
     sln.sample_output_table = sample_output_table_df
     return sln
--- mip_template/constants.py
+++ mip_template/constants.py
@@ -1,11 +1,13 @@
 from enum import StrEnum
 
-APP_OUTPUT_DIR = '/app/output'
+APP_OUTPUT_DIR = "/app/output"
+
 
 class SolverTypes(StrEnum):
-    GUROBI = 'Gurobi'
-    SCIP = 'SCIP'
+    GUROBI = "Gurobi"
+    SCIP = "SCIP"
+
 
 class SampleConstants(StrEnum):
-    FIRST_VALUE = 'Value 1'
-    SECOND_VALUE = 'Value 2'
+    FIRST_VALUE = "Value 1"
+    SECOND_VALUE = "Value 2"
