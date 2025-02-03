b2db82bf155a44f4868694f4c8f517d65ffce468
Eder Pinheiro <eder.pinheiro@mipwise.com>
2025-02-03 18:13:37 -0300

    feature: add dev dependency ruff, and use it to format the whole project.

--- test_mip_template/test_mip_template_pkg.py
+++ test_mip_template/test_mip_template_pkg.py
@@ -9,11 +9,13 @@ import mip_template
 
 cwd = Path(__file__).parent.resolve()
 
-class TestMipMe(unittest.TestCase):
 
+class TestMipMe(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        dat = utils.read_data(f'{cwd}/data/testing_data/testing_data.json', mip_template.input_schema)
+        dat = utils.read_data(
+            f"{cwd}/data/testing_data/testing_data.json", mip_template.input_schema
+        )
         cls.params = mip_template.input_schema.create_full_parameters_dict(dat)
         cls.dat = dat
 
@@ -21,21 +23,31 @@ class TestMipMe(unittest.TestCase):
         utils.check_data(self.dat, mip_template.input_schema)
 
     def test_2_action_data_prep(self):
-        old_sum = self.dat.sample_input_table['Data Field Two'].sum()
+        old_sum = self.dat.sample_input_table["Data Field Two"].sum()
         dat = mip_template.data_prep_solve(self.dat)
-        new_sum = dat.sample_input_table['Data Field Two'].sum()
-        close_enough = isclose(new_sum, self.params['Sample Float Parameter'] * old_sum, rel_tol=1e-2)
+        new_sum = dat.sample_input_table["Data Field Two"].sum()
+        close_enough = isclose(
+            new_sum, self.params["Sample Float Parameter"] * old_sum, rel_tol=1e-2
+        )
         self.assertTrue(close_enough, "Data prep check")
 
     def test_3_main_solve(self):
         sln = mip_template.solve(self.dat)
-        self.assertSetEqual(set(sln.sample_output_table['Data Field']), {'Option 1', 'Option 2'}, 'Main solve check')
+        self.assertSetEqual(
+            set(sln.sample_output_table["Data Field"]),
+            {"Option 1", "Option 2"},
+            "Main solve check",
+        )
 
     def test_4_action_report_builder(self):
         sln = mip_template.solve(self.dat)
-        sln = mip_template.report_builder_solve(self.dat, sln, f'{cwd}/app/output')
-        self.assertSetEqual(set(sln.sample_output_table['Data Field']), {'Option 1.0', 'Option 2.0'}, "Report check")
+        sln = mip_template.report_builder_solve(self.dat, sln, f"{cwd}/app/output")
+        self.assertSetEqual(
+            set(sln.sample_output_table["Data Field"]),
+            {"Option 1.0", "Option 2.0"},
+            "Report check",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
