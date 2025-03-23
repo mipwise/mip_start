7ec3ac23f8db570a83187118b8c515585c7953bf
andrearujano <ajrujano@gmail.com>
2025-03-23 14:55:18 -0300

    feat: update main

--- mip_template/main.py
+++ mip_template/main.py
@@ -1,30 +1,36 @@
 from mwcommons.ticdat_utils import set_data_types, set_parameters_datatypes
-
-from mip_template.create_output_data import create_output_tables
-from mip_template.load_model_data import get_optimization_data
-from mip_template.model import optimize
-from mip_template.schemas import input_schema
+from mip_template import input_schema
+from mip_template import get_optimization_data, optimize, create_output_tables
 
 
 def solve(dat):
     """
     Main solve engine.
-    
+
     Parameters
     ----------
     dat
         Input data, according to input schema.
-    
+
     Returns
     -------
     sln
         Output data, according to output schema.
     """
+
+    # Set data types for input data
     dat = set_data_types(dat=dat, schema=input_schema)
+
+    # Set data types for parameters
     params = input_schema.create_full_parameters_dict(dat)
     params = set_parameters_datatypes(params=params, schema=input_schema)
-    data_in = get_optimization_data(dat=dat, params=params)
-    data_out = optimize(data_in=data_in, params=params)
-    sln = create_output_tables(dat=dat, data_in=data_in, data_out=data_out)
-    
+
+    # Get optimization data
+    model_data = get_optimization_data(dat, params)
+
+    # Build optimization model
+    model_sol = optimize(model_data, params)
+
+    # Populate output tables
+    sln = create_output_tables(dat, model_data, model_sol)
     return sln
