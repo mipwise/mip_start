fb39e4c52c174cd0da9c82ce8667c512c0e765a7
andrearujano <ajrujano@gmail.com>
2025-03-23 14:50:57 -0300

    feat: update schemas

--- mip_template/schemas.py
+++ mip_template/schemas.py
@@ -3,52 +3,78 @@ Defines the input and output schemas of the problem.
 For more details on how to implement and configure data schemas see:
 https://github.com/mipwise/mip-go/tree/main/5_develop/4_data_schema
 """
-from mwcommons.ticdat_types import float_number, non_negative_float, non_negative_integer, text
+from mwcommons.ticdat_types import non_negative_float, non_negative_integer, text
 from ticdat import PanDatFactory
 
-from mip_template.constants import SampleConstants
+from mip_template.constants import Portions
 
-
-# TODO: replace by diet problem schema
-# TODO: add TimeLimit and MipGap as ticdat parameters
 # region INPUT SCHEMA
 input_schema = PanDatFactory(
-    parameters=[['Name'], ['Value']],  # Do not change the column names of the parameters table!
-    sample_input_table=[['Primary Key One', 'Primary Key Two'], ['Data Field One', 'Data Field Two']],
-)
+    # syntax: table_name=[['Primary Key One', 'Primary Key Two'], ['Data Field One', 'Data Field Two']]
+    parameters=[['Name'], ['Value']],
+    foods=[['Food ID'], ['Food Name', 'Per Unit Cost']],
+    nutrients=[['Nutrient ID'], ['Nutrient Name', 'Min Intake', 'Max Intake']],
+    foods_nutrients=[['Food ID', 'Nutrient ID'], ['Quantity']])
 # endregion
 
 # region USER PARAMETERS
-input_schema.add_parameter('Sample Text Parameter', default_value='Any Text', **text())
-input_schema.add_parameter('Sample Two Values Parameter', default_value=SampleConstants.FIRST_VALUE,
-                           **text(strings_allowed=tuple(SampleConstants)))
-input_schema.add_parameter('Sample Float Parameter', default_value=1.5,
-                           **non_negative_float(max=10, inclusive_max=True))
-input_schema.add_parameter('Sample Int Parameter', default_value=1,
-                           **non_negative_integer(max=10, inclusive_max=True))
-input_schema.add_parameter('Sample Date Parameter', default_value='11/21/2022', datetime=True)
+input_schema.add_parameter('Food Portions', default_value=Portions.WHOLE, **text(tuple(Portions)))
+input_schema.add_parameter('Food Cost Multiplier', default_value=1.5, **non_negative_float(max=10, inclusive_max=True))
+input_schema.add_parameter('Time Limit', default_value=None, nullable=True, **non_negative_integer())
+input_schema.add_parameter('Mip Gap', default_value=0.001, **non_negative_float(max=1.0, inclusive_max=False))
 # endregion
 
 # region OUTPUT SCHEMA
 output_schema = PanDatFactory(
-    sample_output_table=[['Primary Key'], ['Data Field']],
-)
+    buy=[['Food ID'], ['Food Name', 'Quantity']],
+    nutrition=[['Nutrient ID'], ['Nutrient Name', 'Quantity']])
 # endregion
 
 # region DATA TYPES AND PREDICATES - INPUT SCHEMA
-# region sample_input_table
-table = 'sample_input_table'
-input_schema.set_data_type(table=table, field='Primary Key One', **text())
-input_schema.set_data_type(table=table, field='Primary Key Two', **text())
-input_schema.set_data_type(table=table, field='Data Field One', **text(strings_allowed=('Option 1', 'Option 2')))
-input_schema.set_data_type(table=table, field='Data Field Two', **float_number())
+
+# region foods
+table = 'foods'
+input_schema.set_data_type(table=table, field='Food ID', **text())
+input_schema.set_data_type(table=table, field='Food Name', **text())
+input_schema.set_data_type(table=table, field='Per Unit Cost', **non_negative_float())
+input_schema.set_default_value(table=table, field='Per Unit Cost', default_value=1.00)
 # endregion
+
+# region nutrients
+table = 'nutrients'
+input_schema.set_data_type(table=table, field='Nutrient ID', **text())
+input_schema.set_data_type(table=table, field='Nutrient Name', **text())
+input_schema.set_data_type(table=table, field='Min Intake', **non_negative_float(), nullable=False)
+input_schema.set_data_type(table=table, field='Max Intake', **non_negative_float(), nullable=True)
+input_schema.add_data_row_predicate(table=table, predicate_name='Min Intake <= Max Intake',
+                                    predicate=lambda row: row['Min Intake'] <= row['Max Intake'])
+# endregion
+
+# region foods_nutrients
+table = 'foods_nutrients'
+for field in ['Food ID', 'Nutrient ID']:
+    input_schema.set_data_type(table=table, field=field, **text())
+input_schema.set_data_type(table=table, field='Quantity', **non_negative_float())
+input_schema.add_foreign_key(native_table=table, foreign_table='foods', mappings=[('Food ID', 'Food ID')])
+input_schema.add_foreign_key(native_table=table, foreign_table='nutrients', mappings=[('Nutrient ID', 'Nutrient ID')])
+# endregion
+
 # endregion
 
 # region DATA TYPES AND PREDICATES - OUTPUT SCHEMA
-# region sample_output_table
-table = 'sample_output_table'
-output_schema.set_data_type(table=table, field='Primary Key', **text())
-output_schema.set_data_type(table=table, field='Data Field', **text())
+
+# region buy
+table = 'buy'
+output_schema.set_data_type(table=table, field='Food ID', **text())
+output_schema.set_data_type(table=table, field='Food Name', **text())
+output_schema.set_data_type(table=table, field='Quantity', **non_negative_float())
 # endregion
+
+# region nutrition
+table = 'nutrition'
+output_schema.set_data_type(table=table, field='Nutrient ID', **text())
+output_schema.set_data_type(table=table, field='Nutrient Name', **text())
+output_schema.set_data_type(table=table, field='Quantity', **non_negative_float())
+# endregion
+
 # endregion
