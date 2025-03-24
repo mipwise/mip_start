f4dedf9d474b596af4ccc2a8e91c349430d7d839
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-24 14:43:16 -0300

    feat: remove unnecessary imports in __init__

--- mip_template/__init__.py
+++ mip_template/__init__.py
@@ -1,11 +1,6 @@
 __version__ = "0.1.0"
 from mip_template.schemas import input_schema, output_schema
 from mip_template.action_update_food_cost import update_food_cost_solve
-
-from mip_template.load_model_data import get_optimization_data
-from mip_template.model import optimize
-from mip_template.create_output_data import create_output_tables
-
 from mip_template.main import solve
 from mip_template.action_report_builder import report_builder_solve
 
