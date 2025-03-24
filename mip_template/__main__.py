3986bc4fcbf79f262ec1eac34fe71741b4e17309
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-24 14:43:39 -0300

    docs: add comment to __main__ about -e argument

--- mip_template/__main__.py
+++ mip_template/__main__.py
@@ -4,8 +4,10 @@ from mip_template.main import solve
 from mip_template.schemas import input_schema, output_schema
 
 
-# When run from the command line, will read/write json/xls/csv/db/sql/mdb files
-# For example, the next command will read from a model stored in input.xlsx and write solution.xlsx.
-#   python -m mip_template -i input.xlsx -o solution.xlsx
+# When run from the command line, will read/write json/xls/csv/db/sql/mdb files.
+# For example, the next command will read from a model stored in input.xlsx and write the solution to output.xlsx.
+#   python -m mip_template -i input.xlsx -o solution.xlsx -e errors.xlsx
+# "-e <errors_file_or_dir>" argument is optional; if used ticdat will check for errors (according to the input schema
+# definition) and store in a xlsx/xls file as separate tabs, or into a directory as separate csv files.
 if __name__ == "__main__":
     standard_main(input_schema, output_schema, solve)
