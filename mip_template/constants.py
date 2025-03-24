9decefd6e205ebe3d771409e06dbb31facf7ffeb
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-24 20:30:59 -0300

    feat: remove constants.SolverTypes

--- mip_template/constants.py
+++ mip_template/constants.py
@@ -4,11 +4,6 @@ from enum import StrEnum
 APP_OUTPUT_DIR = '/app/output'
 
 
-class SolverTypes(StrEnum):
-    GUROBI = 'Gurobi'
-    SCIP = 'SCIP'
-
-
 class Portions(StrEnum):
     WHOLE = 'Ensure whole portions'
     FRACTIONAL = 'Portions can be fractional'
