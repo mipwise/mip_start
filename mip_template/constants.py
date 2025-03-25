8daf8b3853a3942d0de6b26fa4208459533f7a2c
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-25 11:42:12 -0300

    refactor: rename portions constants

--- mip_template/constants.py
+++ mip_template/constants.py
@@ -5,5 +5,5 @@ APP_OUTPUT_DIR = '/app/output'
 
 
 class Portions(StrEnum):
-    WHOLE = 'Ensure whole portions'
-    FRACTIONAL = 'Portions can be fractional'
+    WHOLE = 'Whole'
+    FRACTIONAL = 'Fractional'
