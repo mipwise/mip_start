252015302bf3d74c4e181795951ddffea5d7c08f
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-25 09:27:11 -0300

    feat: include back imports in __init__

--- mip_template/__init__.py
+++ mip_template/__init__.py
@@ -1,13 +1,12 @@
 __version__ = "0.1.0"
-from mip_template.schemas import input_schema, output_schema
-from mip_template.action_update_food_cost import update_food_cost_solve
-
-from mip_template.load_model_data import get_optimization_data
-from mip_template.model import optimize
-from mip_template.create_output_data import create_output_tables
-
-from mip_template.main import solve
 from mip_template.action_report_builder import report_builder_solve
+from mip_template.action_update_food_cost import update_food_cost_solve
+from mip_template.constants import Portions
+from mip_template.create_output_data import create_output_tables
+from mip_template.load_model_data import get_optimization_data
+from mip_template.main import solve
+from mip_template.model import optimize
+from mip_template.schemas import input_schema, output_schema
 
 
 # For a configured deployment on Mip Hub see:
