a4b4aa2abf9cddce7087e87aa1c757afed427f54
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-28 14:08:34 -0300

    fix: update_food_cost_solve action silently overwriting input "dat"

--- mip_template/action_update_food_cost.py
+++ mip_template/action_update_food_cost.py
@@ -4,10 +4,11 @@ from mip_template.schemas import input_schema
 def update_food_cost_solve(dat):
     """Scale food cost by 'Food Cost Multiplier' input parameter"""
     params = input_schema.create_full_parameters_dict(dat)
-    foods = dat.foods.copy()
+    _dat = input_schema.copy_pan_dat(dat)
+    foods = _dat.foods.copy()
     
     foods['Per Unit Cost'] = params['Food Cost Multiplier'] * foods['Per Unit Cost']
     foods = foods.round({'Per Unit Cost': 2})
-    dat.foods = foods
+    _dat.foods = foods
     
-    return dat
+    return _dat
