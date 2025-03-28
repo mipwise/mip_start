6234cc4ea5c9d003951988ec81400eff59c2e8e0
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-28 11:49:59 -0300

    feat: add configured deployment to Mip Hub

--- mip_template/__init__.py
+++ mip_template/__init__.py
@@ -9,5 +9,45 @@ from mip_template.model import optimize
 from mip_template.schemas import input_schema, output_schema
 
 
-# For a configured deployment on Mip Hub see:
-# https://github.com/mipwise/mip-go/tree/main/6_deploy/4_configured_deployment
+# Configured deployment on Mip Hub, see https://github.com/mipwise/mip-go/tree/main/6_deploy/4_configured_deployment
+actions_config = {
+    'Update Food Cost': {
+        'schema': 'input',
+        'engine': update_food_cost_solve,
+        'tooltip': "Update the food cost by the factor inputted in the 'Food Cost Multiplier' parameter"
+    },
+    'Report Builder': {
+        'schema': 'output',
+        'engine': report_builder_solve,
+        'tooltip': "Read the output from the main engine and populate a chart of food-nutrient contributions"
+    }
+}
+
+parameters_config = {
+    'hidden': [],
+    'categories': {'Solver': ['Time Limit', 'Mip Gap']},
+    'order': [],
+    'tooltips': {
+        'Food Cost Multiplier': "Factor by which to multiply the 'Per Unit Cost' column in 'foods' input table",
+        'Time Limit': "Maximum time (in seconds) to run the optimization",
+        'Mip Gap': "Relative MIP gap tolerance for mixed-integer linear programs",
+    }
+}
+
+input_tables_config = {
+    'hidden_tables': [],
+    'categories': {},
+    'order': ['parameters', 'foods', 'nutrients', 'foods_nutrients'],
+    'tables_display_names': {},
+    'columns_display_names': {},
+    'hidden_columns': {}
+}
+
+output_tables_config = {
+    'hidden_tables': [],
+    'categories': {},
+    'order': ['buy', 'nutrition'],
+    'tables_display_names': {},
+    'columns_display_names': {},
+    'hidden_columns': {}
+}
