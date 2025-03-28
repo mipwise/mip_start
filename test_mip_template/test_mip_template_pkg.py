3858ffe7122fc6544486c256bd1c98a08ea23cb8
Luiz Suzana <luiz.suzana@mipwise.com>
2025-03-28 14:10:54 -0300

    feat: update unit tests

--- test_mip_template/test_mip_template_pkg.py
+++ test_mip_template/test_mip_template_pkg.py
@@ -13,7 +13,7 @@ class TestMipMe(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        dat = utils.read_data(f'{cwd}/data/testing_data/testing_data.json', mip_template.input_schema)
+        dat = utils.read_data(f'{cwd}/data/testing_data.xlsx', mip_template.input_schema)
         cls.params = mip_template.input_schema.create_full_parameters_dict(dat)
         cls.dat = dat
 
@@ -21,20 +21,40 @@ class TestMipMe(unittest.TestCase):
         utils.check_data(self.dat, mip_template.input_schema)
 
     def test_2_action_data_prep(self):
-        old_sum = self.dat.sample_input_table['Data Field Two'].sum()
-        dat = mip_template.data_prep_solve(self.dat)
-        new_sum = dat.sample_input_table['Data Field Two'].sum()
-        close_enough = isclose(new_sum, self.params['Sample Float Parameter'] * old_sum, rel_tol=1e-2)
-        self.assertTrue(close_enough, "Data prep check")
+        new_dat = mip_template.update_food_cost_solve(self.dat)
+        
+        old_df = self.dat.foods.copy()
+        new_df = new_dat.foods.copy()
+
+        comparison_df = new_df[['Food ID', 'Per Unit Cost']].merge(
+            old_df[['Food ID', 'Per Unit Cost']],
+            on='Food ID',
+            how='outer',
+            suffixes=(' New', ' Old')
+        )
+
+        # ensure old and new Food ID values match
+        missing_entries = comparison_df['Per Unit Cost Old'].isna() | comparison_df['Per Unit Cost New'].isna()
+        self.assertFalse(missing_entries.any(), "Update food cost action should not change Food IDs")
+
+        # ensure new cost is (Food Cost Multiplier) * old cost
+        comparison_df['Diff'] = abs(
+            comparison_df['Per Unit Cost New']
+            - self.params['Food Cost Multiplier'] * comparison_df['Per Unit Cost Old']
+        )
+        close_enough = (comparison_df['Diff'] < 1e-2)
+        self.assertTrue(close_enough.all(), "Update food cost check 2")
 
     def test_3_main_solve(self):
         sln = mip_template.solve(self.dat)
-        self.assertSetEqual(set(sln.sample_output_table['Data Field']), {'Option 1', 'Option 2'}, 'Main solve check')
+        kpis_df = sln.kpis.copy()
+        self.assertTrue("Total Cost" in kpis_df['Name'].values, "'Total Cost' should be a kpi")
+        total_cost = kpis_df.loc[kpis_df['Name'] == 'Total Cost', 'Value'].iloc[0]
+        self.assertTrue(isclose(total_cost, 11.92, abs_tol=1e-2), "'Total Cost' should be 11.92")
 
     def test_4_action_report_builder(self):
         sln = mip_template.solve(self.dat)
         sln = mip_template.report_builder_solve(self.dat, sln, f'{cwd}/app/output')
-        self.assertSetEqual(set(sln.sample_output_table['Data Field']), {'Option 1.0', 'Option 2.0'}, "Report check")
 
 
 if __name__ == '__main__':
